2656f0c4fd39ccda108e2407a8f72d99fa908335
Mohamed Sayed <109354903+MohamedHbo@users.noreply.github.com>
2024-04-30 01:59:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+let search = document.querySelector('#serach-bar');
+let searchbox = document.querySelector('.search-box');
+let menubar = document.querySelector('#menu-bars');
+let mynav = document.querySelector('.navbar');
+
+
+
+
+search.onclick = () =>{
+    searchbox.classList.toggle('active');
+}
+
+menubar.onclick = () =>{
+    menubar.classList.toggle('fa-times')
+    mynav.classList.toggle('active')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let search = document.querySelector('#serach-bar');
+// let searchbox =document.querySelector('.search-box');
+
+// search.onclick = () =>{
+//     searchbox.classList.toggle('active');
+// }
